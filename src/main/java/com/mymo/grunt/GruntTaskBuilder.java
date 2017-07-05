e86927325c2734f7b0f1a2ebfcfaa97c4ab2c5df
phil.madden <philmadden83@gmail.com>
2017-07-05 11:08:06 -0500

    initial commit

--- src/main/java/com/mymo/grunt/GruntTaskBuilder.java
+++ src/main/java/com/mymo/grunt/GruntTaskBuilder.java
@@ -2,45 +2,132 @@ package com.mymo.grunt;
 
 import hudson.Launcher;
 import hudson.Extension;
-import hudson.FilePath;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.Run;
-import hudson.model.TaskListener;
+import hudson.model.BuildListener;
 import hudson.tasks.Builder;
 import hudson.tasks.BuildStepDescriptor;
-import jenkins.tasks.SimpleBuildStep;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.IOException;
 
-public class GruntTaskBuilder extends Builder implements SimpleBuildStep {
+public class GruntTaskBuilder extends Builder {
+    private static final String DEBUG_FLAG      = "debug";
+    private static final String FORCE_FLAG      = "force";
+    private static final String BASE_FLAG       = "flag";
+    private static final String VERBOSE_FLAG    = "verbose";
+    private static final String GRUNTFILE_FLAG  = "gruntfile";
+
     private final String task;
+    private final String base;
+    private final String gruntFile;
+    private final String properties;
+
+    private final boolean debug;
+    private final boolean force;
+    private final boolean verbose;
 
     @DataBoundConstructor
-    public GruntTaskBuilder(String task) {
-        this.task = task;
+    public GruntTaskBuilder(String task, String base, String gruntFile, String properties,
+                            boolean debug, boolean force, boolean verbose) {
+        this.task       = task;
+        this.base       = base;
+        this.gruntFile  = gruntFile;
+        this.properties = properties;
+        this.debug      = debug;
+        this.force      = force;
+        this.verbose    = verbose;
     }
 
     @Override
-    public void perform(Run<?,?> build, FilePath workspace, Launcher launcher, TaskListener listener) {
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
         try {
-            launcher.launch()
-                    .cmdAsSingleString(String.format("%s %s", getDescriptor().getGruntHome(), task))
+
+            int result = launcher.launch()
+                    .cmdAsSingleString(getCommand())
                     .join();
+
+            return result == 0;
+
         } catch (IOException | InterruptedException e) {
-            listener.getLogger().append(e.getMessage());
+            listener.fatalError(e.getMessage());
         }
+
+        return false;
+    }
+
+    private String getCommand() {
+        StringBuilder stringBuilder = new StringBuilder(String.format("%s %s", getDescriptor().getGruntHome(), task));
+
+        if (base != null && !base.isEmpty()) {
+            appendFlag(stringBuilder, BASE_FLAG, base);
+        }
+
+        if (gruntFile != null && !gruntFile.isEmpty()) {
+            appendFlag(stringBuilder, GRUNTFILE_FLAG, gruntFile);
+        }
+
+        if (properties != null && !properties.isEmpty()) {
+            for (String propArg : properties.split("\n")) {
+                appendFlag(stringBuilder, propArg);
+            }
+        }
+
+        if (debug) {
+            appendFlag(stringBuilder, DEBUG_FLAG);
+        }
+
+        if (force) {
+            appendFlag(stringBuilder, FORCE_FLAG);
+        }
+
+        if (verbose) {
+            appendFlag(stringBuilder, VERBOSE_FLAG);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private static void appendFlag(final StringBuilder builder, final String flag, final String... opts) {
+        builder.append(String.format(" --%s", flag));
+        if (opts != null && opts.length > 0) {
+            builder.append(String.format(" %s", opts));
+        }
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public String getGruntFile() {
+        return gruntFile;
+    }
+
+    public String getProperties() {
+        return properties;
+    }
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public boolean isForce() {
+        return force;
+    }
+
+    public boolean isVerbose() {
+        return verbose;
     }
 
     @Override
     public GruntTaskBuildStepDescriptor getDescriptor() {
         return (GruntTaskBuildStepDescriptor) super.getDescriptor();
-    }
-
-    public String getTask() {
-        return task;
     }
 
     @Extension
